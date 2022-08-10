beac421629eb4f79d21f658e6e6b7261217fe1cb
orange2000121 <danny1.2104@gmail.com>
2022-08-10 19:13:23 +0800

    splah scream ,login page

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -13,26 +13,102 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('loginPage'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
-              keyboardType: TextInputType.emailAddress,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("resources/splash_background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: const Color.fromARGB(65, 112, 112, 112)),
+              color: const Color.fromARGB(90, 255, 255, 255),
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
             ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
+            width: MediaQuery.of(context).size.width * 0.88,
+            height: MediaQuery.of(context).size.height * 0.69,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('忘記帳號密碼 / 註冊帳號',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: '帳號',
+                      hintStyle: TextStyle(fontSize: 18),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30)), borderSide: BorderSide(color: Color(0xff707070))),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: '密碼',
+                      hintStyle: TextStyle(fontSize: 18),
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30)), borderSide: BorderSide(color: Color(0xff707070))),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 300,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: const Color(0xff707070)),
+                      borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    ),
+                    child: const Center(
+                      child: Text('登入',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 80,
+                        height: 80,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(image: AssetImage('resources/button/apple__4_-removebg-preview.png')),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 80,
+                        height: 80,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(image: AssetImage('resources/button/google__1_-removebg-preview.png')),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -7,13 +7,28 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF333333),
-      body: Center(
-        child: Icon(
-          Icons.pets,
-          size: MediaQuery.of(context).size.width * 0.7,
-        ),
-      ),
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage("resources/splash_background.png"), fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'resources/splash_logo.png',
+                  height: MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                Row(
+                  children: const [
+                    SizedBox(width: 150),
+                    Text('標語................', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
